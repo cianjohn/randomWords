d48699dca3d0592838c947f28c2210b56a6f62bc
Gitish98 <ismailiswell@gmail.com>
2021-05-10 11:21:47 +0100

    new function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 var words = ["orange","apple","nandos","stairs","corridoor","house","doge","mcdonalds","batman"];
- 
+var userInput = document.querySelector("#userInput")
 
 function randomWordFunction(){
-  
-    let userInput = parseInt(prompt("Enter a number between 1 and 10"));
     for( i=0;i<userInput;i++ ){
     let randomnumber = Math.floor(Math.random() * words.length);
     let temporaryRandomWord = words.splice(randomnumber, 1);
-    console.log(temporaryRandomWord)
     }
-    console.log(words)
 }
+
+
 
 
 
