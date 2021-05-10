3625ae81bf375258c0c6efa1171a7b806308316d
Gitish98 <ismailiswell@gmail.com>
2021-05-10 13:56:07 +0100

    draft

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
 var words = ["orange","apple","nandos","stairs","corridoor","house","doge","mcdonalds","batman"];
-let masterwords = []
 let button = document.querySelector("button");
+let box1 = document.querySelector("#list")
+let reshuffle = document.querySelector("#reshuffle")
+
+// Random words
 button.addEventListener("click", function(){  
+    let masterwords = []
     let usersInput = document.querySelector("#userInput").value;
     for( i=0;i<usersInput;i++ ){
     let randomnumber = Math.floor(Math.random() * words.length);
     let temporaryRandomWord = words.splice(randomnumber, 1);
-    console.log(temporaryRandomWord)
+    masterwords.push(temporaryRandomWord[0])
     }
-    console.log(words)
+    for (word of masterwords){
+        box1.innerHTML+=`<p>${word}</p>`
+    }
+    return usersInput;
+    
+
+});
+
+
+reshuffle.addEventListener("click", function(){
+    box1.innerHTML = " "
+    let masterwords = []
+    let usersInput = document.querySelector("#userInput").value;
+    for( i=0;i<usersInput;i++ ){
+    let randomnumber = Math.floor(Math.random() * words.length);
+    let temporaryRandomWord = words.splice(randomnumber, 1);
+    masterwords.push(temporaryRandomWord[0])
+    }
+    for (word of masterwords){
+        box1.innerHTML+=`<p>${word}</p>`
+    }
+    return usersInput;
+
 })
+
+
+
+
