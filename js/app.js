2805964c4221358fe46d7a288846a9755898cd8f
Gitish98 <ismailiswell@gmail.com>
2021-05-10 12:45:36 +0100

    Got input working

--- js/app.js
+++ js/app.js
@@ -1,32 +1,12 @@
 var words = ["orange","apple","nandos","stairs","corridoor","house","doge","mcdonalds","batman"];
-var userInput = document.querySelector("#userInput")
-
 let masterwords = []
-
 let button = document.querySelector("button");
-button.addEventListener("click", function(){
-    // let temo =
-    let userInput = parseInt(document.querySelector("#userInput").nodeValue);
-    let randomWords = []
-    for(i=0; i<userinput; i++){
-         randomWords.push(randomword())
-    } 
-
-
-    return console.log(randomWords)
-
-})
-
-
-function randomWordFunction(){
-    for( i=0;i<userInput;i++ ){
+button.addEventListener("click", function(){  
+    let usersInput = document.querySelector("#userInput").value;
+    for( i=0;i<usersInput;i++ ){
     let randomnumber = Math.floor(Math.random() * words.length);
     let temporaryRandomWord = words.splice(randomnumber, 1);
+    console.log(temporaryRandomWord)
     }
-}
-
-
-
-
-
-randomWordFunction()
+    console.log(words)
+})
