a8739274c9da2433c92a567803f6317747f8c76c
cianjohn <83083026+cianjohn@users.noreply.github.com>
2021-05-10 11:09:37 +0100

    got input working and page started

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let masterwords = []
+let button = document.querySelector("button");
+button.addEventListener("click", function(){
+    // let temo =
+    let userInput = parseInt(document.querySelector("#cars").nodeValue);
+    let randomWords = []
+    for(i=0; i<userinput; i++){
+         randomWords.push(randomword())
+    } 
+
+
+    return console.log(randomWords)
+
+})
+
